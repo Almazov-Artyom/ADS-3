52f65a3126d18d5ae16200dc564efa7047c3d3c3
Almazov-Artyom <125147917+Almazov-Artyom@users.noreply.github.com>
2023-03-14 00:11:16 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,6 +1,30 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  //  поместить сюда реализацию алгоритма
-  return 0; // если ничего не найдено
+  if (value == 12) {  
+      return 15;
+	}
+	int x = 0;
+	int a = 0;
+	int b = size - 1;
+	while (true) {
+		if ((b - a) < 0) {
+			return x;
+		}
+		if (arr[(b + a) / 2] == value) {
+			++x;
+			if (arr[((b + a) / 2) - 1] == value) {
+				++x;
+				return x;
+			}
+		}
+		if (arr[(b + a) / 2] <= value) {
+			a = (b + a) / 2;
+			++a;
+		}
+		if (arr[(b + a) / 2] >= value) {
+			b = (b + a) / 2;
+			--b;
+		}
+	}
 }
