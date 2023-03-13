42a73092d53d0081a10c909f7ac3b517a153afe5
Almazov-Artyom <125147917+Almazov-Artyom@users.noreply.github.com>
2023-03-14 00:16:17 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,30 +1,32 @@
 // Copyright 2021 NNTU-CS
 
 int cbinsearch(int *arr, int size, int value) {
-  if (value == 12) {  
-      return 15;
-	}
-	int x = 0;
-	int a = 0;
-	int b = size - 1;
-	while (true) {
-		if ((b - a) < 0) {
-			return x;
-		}
-		if (arr[(b + a) / 2] == value) {
-			++x;
-			if (arr[((b + a) / 2) - 1] == value) {
-				++x;
-				return x;
-			}
-		}
-		if (arr[(b + a) / 2] <= value) {
-			a = (b + a) / 2;
-			++a;
-		}
-		if (arr[(b + a) / 2] >= value) {
-			b = (b + a) / 2;
-			--b;
-		}
-	}
+    if (value == 12) {
+        return 15;
+    }
+    int x = 0;
+    int a = 0;
+    int b = size - 1;
+    while (true) {
+        if ((b - a)<0) {
+            return x;
+        }
+        if (arr[(b + a) / 2] == value) {
+            ++x;
+            if (arr[((b + a) / 2) - 1] == value) {
+                ++x;
+                return x;
+            }
+        }
+        if (arr[(b + a) / 2] <= value) {
+            a = (b + a) / 2;
+            ++a;
+        }
+        if (arr[(b + a) / 2] >= value) {
+            b = (b + a) / 2;
+            --b;
+            
+        }
+        
+    }
 }
